cf0015d393ac4cb44a5c55648d9d57fc4b3ffde8
Shota Ito <shota.110.free.style@gmail.com>
2021-07-28 23:20:08 -0400

    Atoms, Molecules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>Test</PrimaryButton>
+      <SecondaryButton>Search</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
