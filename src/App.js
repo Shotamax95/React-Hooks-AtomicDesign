822d739fc63469dff1412bac015d10548ed12a51
Shota Ito <shota.110.free.style@gmail.com>
2021-08-05 14:08:14 -0400

    Atomic Design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import "./styles.css";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
+import { Router } from "./router/Router";
 
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>Test</PrimaryButton>
-      <SecondaryButton>Search</SecondaryButton>
-      <br />
-      <SearchInput />
-    </div>
-  );
+  return <Router />;
 }
